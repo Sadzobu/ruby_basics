1e6ef6cdb06a84786df742dbfa3b7dc7e3ab36d0
Sadzobu <kaonokage@gmail.com>
2021-10-07 07:29:33 -0700

    added set_route for interface

--- lesson_4/main.rb
+++ lesson_4/main.rb
@@ -17,14 +17,19 @@ t2 = PassengerTrain.new("322")
 
 s1 = Station.new("Domo")
 s2 = Station.new("Avto")
+s3 = Station.new("Kolomna")
+s4 = Station.new("Ramenki")
 
-r = Route.new(s1, s2)
+r1 = Route.new(s1, s2)
+r2 = Route.new(s4,s3)
+r2.add_station(s1)
+r2.add_station(s2)
 
-t1.set_route(r)
-t2.set_route(r)
+t1.set_route(r1)
+t2.set_route(r2)
 trains << t1 << t2
-stations << s1 << s2
-routes << r
+stations << s1 << s2 << s3 << s4
+routes << r1 << r2
 #test data end
 
 while true
@@ -34,9 +39,9 @@ while true
   puts "Please, choose one of the options below:"
   puts "Type 'C', to Create new object (Train, Station, Route)"
   puts "Type 'U', to update object (Train, Station)"
-  puts "Type 'S', to Show information about object (Station, Route)"
+  puts "Type 'S', to Show information about object (Station)"
   puts "Type 'E', to Exit"
-  user_input = gets.chomp()
+  user_input = gets.chomp
   case user_input
   when 'C'
     puts "\e[H\e[2J" 
@@ -44,19 +49,19 @@ while true
     puts "Type 'T', to create new Train"
     puts "Type 'S', to create new Station"
     puts "Type 'R', to create new Route"
-    user_input = gets.chomp()
+    user_input = gets.chomp
     case user_input
     when 'T'
       puts "\e[H\e[2J"
       puts "Please, choose a train type:"
       puts "1: Cargo"
       puts "2: Passenger"
-      user_input = gets.chomp()
+      user_input = gets.chomp
       case user_input
       when '1'
         puts "\e[H\e[2J"
         puts "Please, enter train number:"
-        user_input = gets.chomp()
+        user_input = gets.chomp
         trains.append(CargoTrain.new(user_input))
         puts "Cargo train with number #{user_input} created!"
         puts "Press anything to continue:"
@@ -64,7 +69,7 @@ while true
       when '2'
         puts "\e[H\e[2J"
         puts "Please, enter train number:"
-        user_input = gets.chomp()
+        user_input = gets.chomp
         trains.append(PassengerTrain.new(user_input))
         puts "Passenger train with number #{user_input} created!"
         puts "Press anything to continue:"
@@ -73,7 +78,7 @@ while true
     when 'S'
       puts "\e[H\e[2J"
       puts "Please, enter station name:"
-      user_input = gets.chomp()
+      user_input = gets.chomp
       stations.append(Station.new(user_input))
       puts "Station \"#{user_input}\" was created"
       puts "Press anything to continue:"
@@ -81,7 +86,7 @@ while true
     when 'R'
       puts "\e[H\e[2J"
       puts "Please, enter route's first station name:"
-      user_input = gets.chomp()
+      user_input = gets.chomp
       start_station = stations.find {|station| station.name == user_input}
       if start_station.nil?
         puts "This station does not exist, can't create route"
@@ -90,7 +95,7 @@ while true
         next
       end
       puts "Please, enter route's  last station name:"
-      user_input = gets.chomp()
+      user_input = gets.chomp
       end_station = stations.find {|station| station.name == user_input}
       if end_station.nil?
         puts "This station does not exist, can't create route"
@@ -108,7 +113,65 @@ while true
     puts "Enter 'T', to add or remove cars from train"
     puts "Enter 'S', to add or remove stations from route"
     puts "Enter 'M', to move train on the route"
-    user_input = gets.chomp()
+    user_input = gets.chomp
+    case user_input
+    when 'T'
+      puts "\e[H\e[2J"
+      puts "Enter train number:"
+      user_input = gets.chomp
+      current_train = trains.find {|train| train.id == user_input}
+      if current_train.nil?
+        puts "This train does not exist"
+        puts "Press anything to continue:"
+        user_input = gets
+        next
+      end
+      puts "Press 'A', to add car to train #{current_train.id}"
+      puts "Press 'D', to remove car from train #{current_train.id}"
+      user_input = gets.chomp
+      case user_input
+      when 'A'
+        if current_train.type == :cargo
+          current_train.add_car(CargoCar.new())
+        else
+          current_train.add_car(PassengerCar.new())
+        end
+        puts "Train #{current_train.id} now has #{current_train.cars.length} cars!"
+        puts "Press anything to continue:"
+        user_input = gets
+      when 'D'
+        if current_train.type == :cargo
+          current_train.del_car(CargoCar.new())
+        else
+          current_train.del_car(PassengerCar.new())
+        end
+        puts "Train #{current_train.id} now has #{current_train.cars.length} cars!"
+        puts "Press anything to continue:"
+        user_input = gets
+      end
+    when 'S'
+    when 'M'
+      puts "\e[H\e[2J"
+      puts "Enter train number:"
+      user_input = gets.chomp
+      current_train = trains.find {|train| train.id == user_input}
+      if current_train.nil?
+        puts "This train does not exist"
+        puts "Press anything to continue:"
+        user_input = gets
+        next
+      end
+      puts "Choose a route for the train:"
+      routes.each_with_index do |route, i|
+        print "#{i}: "
+        route.show_stations
+      end
+      user_input = gets.chomp.to_i
+      current_train.set_route(routes[user_input])
+      puts "Train #{current_train.id} is now on route #{user_input.to_s}!"
+      puts "Press anything to continue:"
+      user_input = gets
+    end
   when 'S'
     puts "\e[H\e[2J"
     puts "Current stations are:"
